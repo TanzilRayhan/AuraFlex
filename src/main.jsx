1123ff8e777303b5cfbb0deb17c295293932c827
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-11-23 22:43:10 +0600

    Basic setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+/* eslint-disable no-unused-vars */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HelmetProvider } from 'react-helmet-async';
+import ErrorPage from "./pages/ErrorPage";
+import Main from "./layout/Main";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+     
+    ]
+    
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  </React.StrictMode>
+);
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/no-unknown-property */
+
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+    return (
+      <div className="max-w-xl max-h-fit mx-auto">
+        <h1 className="font-bold text-5xl text-center mt-10">Page Not Found</h1>
+        <div className=" my-5 flex justify-center items-center">
+        <Link to="/"><button className="btn btn-info font-bold text-xl text-center ">Back to home</button></Link>
+        </div>
+        <img className="flex justify-center items-center" src="https://i.ibb.co/txLM9J7/Error-404.jpg" alt="error 404"  />
+      </div>
+    );
+  };
+  
+  export default ErrorPage;
+  
